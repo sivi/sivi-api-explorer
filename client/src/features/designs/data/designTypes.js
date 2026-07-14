663c2cb6c46a791c61dd792abddff1a2f0edcc90
Ram 3.0 <ram@hellosivi.com>
2026-07-14 09:19:34 +0530

    3h

--- client/src/features/designs/data/designTypes.js
+++ client/src/features/designs/data/designTypes.js
@@ -320,7 +320,6 @@ export const composeModels = {
   'sivi-gen-3h-preview': { label: 'Sivi Gen-3H Pro Preview' },
   'sivi-gen-3h-max-preview': { label: 'Sivi Gen-3H Max Preview' },
   'sivi-gen-3h-lite': { label: 'Sivi Gen-3H Lite' },
-  'sivi-gen-3h': { label: 'Sivi Gen-3H Pro' },
   'sivi-gen-3h-max': { label: 'Sivi Gen-3H Max' },
   'sivi-gen-28-vlg': { label: 'Sivi Gen-2.8 VLG' },
   'sivi-gen-27': { label: 'Sivi Gen-2.7' },
